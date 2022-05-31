216921ae6b96ec4f1a1f250be03255c77b8e65fe
Han Li <vcfvct@gmail.com>
2022-05-31 00:50:27 -0400

    init

---
+++ src/sermon-list/SermonList.tsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect, useMemo, useCallback} from 'react';
+import { parse, ParseResult } from 'papaparse';
+import { AgGridReact } from 'ag-grid-react'; // the AG Grid React Component
+
+import 'ag-grid-community/dist/styles/ag-grid.css'; // Core grid CSS, always needed
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css'; // Optional theme 
+import { CellClickedEvent } from 'ag-grid-community';
+
+export const SermonList = () => {
+
+  const [sermons, setSermons] = useState<Array<PodCastMeta>>([]);
+
+  useEffect(() => {
+    parse('/sermons.csv', {
+      download: true,
+      header: true,
+      dynamicTyping: true,
+      complete: (rs: ParseResult<PodCastMeta>) => {
+        setSermons(rs.data);
+      }
+    })
+
+  }, [])
+
+
+  // Each Column Definition results in one Column.
+  const [columnDefs, setColumnDefs] = useState([
+    { field: 'title', filter: true, sortable: false},
+    { field: 'author', filter: 'agSetColumnFilter', },
+    { field: 'eventName', filter: true},
+    { field: 'eventDate', filter: true},
+    { field: 'description', sortable: false},
+  ]);
+
+  // DefaultColDef sets props common to all Columns
+  const defaultColDef = useMemo(() => ({
+    sortable: true
+  }), []);
+
+  // Example of consuming Grid Event
+  const cellClickedListener = useCallback((e: CellClickedEvent) => {
+    console.log(e)
+  }, []);
+
+  return (
+    <>
+
+      {/* On div wrapping Grid a) specify theme CSS Class Class and b) sets Grid size */}
+      <div className="ag-theme-alpine" style={{ maxWidth: 1024, height: '100%', margin: '0 auto'}}>
+        <AgGridReact
+          rowData={sermons} // Row Data for Rows
+          columnDefs={columnDefs} // Column Defs for Columns
+          defaultColDef={defaultColDef} // Default Column Properties
+          animateRows={true} // Optional - set to 'true' to have rows animate when sorted
+          rowSelection='multiple' // Options - allows click selection of rows
+          pagination={true}
+          onCellClicked={cellClickedListener} // Optional - registering for Grid Event
+        />
+      </div>
+    </>
+  );
+}
+
+export interface PodCastMeta {
+  id: number;
+  title: string;
+  body: string;
+  author: string;
+  eventDate: string;
+  eventName: string;
+  audio?: string;
+  video?: string;
+  youtubeUrl?: string;
+  description?: string;
+}
+
