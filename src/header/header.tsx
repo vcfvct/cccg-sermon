170a3c569a2ebae2abeb7bf9d7cb2914f4b0ecbb
Han Li <vcfvct@gmail.com>
2022-06-11 14:45:51 -0400

    responsive header image/text

--- src/header/header.tsx
+++ src/header/header.tsx
@@ -5,14 +5,14 @@ export const HeaderComponent = () => {
   const homeUrl = 'https://www.cccgermantown.org';
   return (
     <div className='app-header'>
-      <a href={homeUrl} target="blank">
-        <img src='./cccg_logo_50x50_trans.webp' alt='logo' />
+      <a href={homeUrl} target="blank" style={{ height: '100%' }}>
+        <img src='./cccg_logo_50x50_trans.webp' alt='logo' style={{ height: '100%', width: 'auto' }} />
       </a>
-      <h1>
-        <a className='header-link' href={homeUrl} target="blank">基督教德国镇中国教会</a>
-        <br />
-        <a className='header-link' href={homeUrl} target="blank">Chinese Christian Church of Germantown</a>
-      </h1>
+      <a className='header-link' href={homeUrl} target="blank">
+        基督教德国镇中国教会
+          <br />
+          Chinese Christian Church of Germantown
+      </a>
     </div>
   )
 };
