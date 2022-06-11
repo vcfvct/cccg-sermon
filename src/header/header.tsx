94f4a72a2b1020d330485943b83c43272baf1aac
Han Li <vcfvct@gmail.com>
2022-06-11 01:04:46 -0400

    abstract home url

--- src/header/header.tsx
+++ src/header/header.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import './header.css'
 
 export const HeaderComponent = () => {
+  const homeUrl = 'https://www.cccgermantown.org';
   return (
     <div className='app-header'>
-      <a href="https://www.cccgermantown.org" target="blank">
+      <a href={homeUrl} target="blank">
         <img src='./cccg_logo_50x50_trans.webp' alt='logo' />
       </a>
       <h1>
-        <a className='header-link' href="https://www.cccgermantown.org" target="blank">基督教德国镇中国教会</a>
+        <a className='header-link' href={homeUrl} target="blank">基督教德国镇中国教会</a>
         <br />
-        <span><a className='header-link' href="https://www.cccgermantown.org" target="blank">Chinese Christian Church of Germantown</a></span>
+        <a className='header-link' href={homeUrl} target="blank">Chinese Christian Church of Germantown</a>
       </h1>
     </div>
   )
